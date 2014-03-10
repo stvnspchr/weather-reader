7b1d3c42bb54504117eb5ae1830b9abefefcf413
Steven Speicher <stevenspeicher@gmail.com>
2014-03-10 16:35:10 -0500

    Add variable date range

--- reader.py
+++ reader.py
@@ -2,14 +2,15 @@ import csv
 import urllib2
 import StringIO
 import os
-# 
-# begin-month = 1
-# end-month = 3
-# begin-day = 1
-# end-day = 10
 
-for m in range(1,5):				# Months
-	for d in range(1,3):			# Days
+# Set date range for weather data pull
+begin_month = 1
+end_month = 3
+begin_day = 1
+end_day = 10
+
+for m in range(begin_month,end_month):				# Months
+	for d in range(begin_day,end_day):				# Days
 		# Open webpage and read data
 		url = 'http://www.wunderground.com/history/airport/KMLE/2014/' + str(m) + '/' + str(d) + '/DailyHistory.html?req_city=Ashland&req_state=NE&req_statename=Nebraska&format=1'
 		print "reading webpage on " + str(m) + '/' + str(d)
